a22821a29a123d9dc75f6e0e304d3bb53b735aec
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-09-22 11:04:12 -0300

    Update Cards.php

--- Cards.php
+++ Cards.php
@@ -7,7 +7,7 @@ class Carta{
 
     private string $naipe;
 
-    public function getNumero(): int
+    public function getNumero  (): int
     {
         return $this->numero;
     }
@@ -53,18 +53,19 @@ function sortearCarta($baralho){
 }
 
 $frances = [
-        "Paus" => array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Reis"),
-        "Ouro" => array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Reis"),
-        "Espada" => array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Reis"),
-        "Copas" => array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Reis"),
+        "Paus" => array("Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Reis"),
+        "Ouro" => array("Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Reis"),
+        "Espada" => array("Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Reis"),
+        "Copas" => array("Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Reis"),
 ];
-
-
-
 
 
 $baralho = array();
 $cartaSorteada = null;
+$numEscolha = "";
+$naipeEscolha = "";
+$pontuacao = 0;
+$tentativasErradas = 0;
 
 //Gerar o baralho de cartas e a carta sorteada
 do{
@@ -72,8 +73,8 @@ do{
     echo str_repeat("=", 42) . "\n";
     echo "      BEM-VINDO AO JOGO DE ADIVINHAÇÃO      \n";
     echo str_repeat("=", 42) . "\n";
-    echo "          1 - Baralho Francês (tradcicional)\n";
-    echo "          0 - Encerrar o Programa\n";
+    echo "      1 - Baralho Francês (tradcicional)\n";
+    echo "      0 - Encerrar o Programa\n";
     echo str_repeat("=", 42) . "\n";
 
     $escolha = readline("Escolha uma opção: ");
@@ -104,7 +105,48 @@ do{
                 echo $naipe . " -> " . $valoresGeral . "\n";
             }
             $cartaSorteada = sortearCarta($baralho);
-            echo "A carta foi sorteada tente adivinhá-la!!";
+            echo "\nA carta foi sorteada tente adivinhá-la!!";
+
+                echo "\n";
+                echo str_repeat("=", 42) . "\n";
+                echo "      1 - Tentar adivinhar\n";
+                echo "      0 - Desistir   \n";
+                echo str_repeat("=", 42) . "\n";
+                $choice = readline("Continuar ou desistir: ");
+
+                echo "\nNo momento sua pontuação é $pontuacao.\n\n";
+
+                /// Para fins de teste professor, se quiser saber se está validando quando acertar
+                echo "Teste para ver a validação do acerto\n";
+                print_r($cartaSorteada);
+                
+                if ($choice == 1){
+                    while($naipeEscolha != 0 or $naipeEscolha != $cartaSorteada->getNaipe() and $numEscolha != $cartaSorteada->getNome()){
+                    echo str_repeat("=", 42) . "\n";
+                    $naipeEscolha = readline("Naipe (digite 0 para encerrar o programa): ");
+
+                        if($naipeEscolha == 0){
+                            echo "A carta sorteada foi " . $cartaSorteada->getNome() . " de " . $cartaSorteada->getNaipe() . ".\n";
+                            break;
+                        }
+
+                        $numEscolha = readline("Numero/Nome: ");
+
+                        if($naipeEscolha == $cartaSorteada->getNaipe() and $numEscolha == $cartaSorteada->getNome()){
+                            echo "PARABÉNSSSS!!! Você acertou.\n";
+                            $pontuacao += 1;
+                            echo "Sua pontuação agora é $pontuacao.\n";
+                            break;
+                        }
+                        else{
+                            $tentativasErradas += 1;
+                            echo "ERRADO!!! Tente novamente.\n";
+                            if($tentativasErradas == 3){
+                                echo "Dica: o naipe é " . $cartaSorteada->getNaipe() . ".\n";
+                            }
+                        }
+                    }
+                }
             break;
 
         default:
@@ -112,6 +154,4 @@ do{
             break;
     }
 }while($escolha != 0);
-
-//Rotina para a advinhação da carta
 
