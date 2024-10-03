4bacaa1dd615d74d2ddd36695be900c7899a0b86
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-10-03 15:48:50 -0300

    Create Prato.php

---
+++ AtividadePrato/modelo/Prato.php
@@ -0,0 +1,72 @@
+<?php 
+
+class Prato{
+
+    private int $numero;
+    private string $nome;
+    private float $valor;
+
+
+    public function __construct($numero, $nome, $valor)
+    {
+        $this->numero = $numero;
+        $this->nome = $nome;
+        $this->valor = $valor;
+        
+    }
+
+
+    /**
+     * Get the value of numero
+     */
+    public function getNumero(): int
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set the value of numero
+     */
+    public function setNumero(int $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of valor
+     */
+    public function getValor(): float
+    {
+        return $this->valor;
+    }
+
+    /**
+     * Set the value of valor
+     */
+    public function setValor(float $valor): self
+    {
+        $this->valor = $valor;
+
+        return $this;
+    }
+}
