60c6679488cecd377f00fa9ed1e7f532fee396c0
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-11-08 14:24:03 -0300

    Update execucao.php

--- AtivEspaco/execucao.php
+++ AtivEspaco/execucao.php
@@ -8,8 +8,8 @@ require_once('modelo/PontoTuristico.php');
 
 $espacos = array();
 
-function cadastrarEspaco() {
-    echo "CADASTRAR ESPAÇO";
+function cadastrarEspaco(&$espacos) {
+    echo "CADASTRAR ESPAÇO\n";
     print("╔═══════════════ ESPAÇO ══════════════╗\n");
     print("║     O que deseja fazer?             ║\n");
     print("║ 1: Hotel                            ║\n");
@@ -18,40 +18,100 @@ function cadastrarEspaco() {
     print("║ 0: Cancelar                         ║\n");
     print("╚═════════════════════════════════════╝\n");
 
-    $espacoEscolha = readline("Qual tipo de espaço deseja cadastrar: ");
 
-    if ($espacoEscolha = 1) {
-        $hotel = new Hotel();
-        $hotel->setNome(readline("Nome: "))
-              ->setEndereco(readline("Endereço: "));
-        $hotel->setNumEstrelas(readline("Estrelas: "));
+    while (true) {  
+        $espacoEscolha = readline("Qual tipo de espaço deseja cadastrar: ");
 
-        $cafeIncluso = readline("Café incluso (S/N): ");
-        if ($cafeIncluso == "S" || $cafeIncluso == "s") {
-            $hotel->setCafeIncluso(true);
-        } else {
-            $hotel->setCafeIncluso(false);
+
+        if ($espacoEscolha == 1) {
+            $hotel = new Hotel();
+            $hotel->setNome(readline("Nome: "))
+                ->setEndereco(readline("Endereço: "));
+            $hotel->setNumEstrelas(readline("Número de estrelas: "));
+
+            $cafeIncluso = readline("Café incluso (S/N): ");
+            if ($cafeIncluso == "Sim" || $cafeIncluso == "sim") {
+                $hotel->setCafeIncluso(true);
+            } else {
+                $hotel->setCafeIncluso(false);
+            }
+            array_push($espacos, $hotel);
+            break;
         }
-        array_push($espacos, $hotel);
-    }
 
-    elseif ($espacoEscolha = 2){
-        $restaurante = new Restaurante();
-        $restaurante->setNome(readline("Nome: "))
-                    ->setEndereco(readline("Endereço: "));
-        $restaurante->setTipoComida(readline("Tipo de comida: "));
-        array_push($espacos, $restaurante);
-    }
-
-    elseif ($espacoEscolha = 3){
-        $pontoTuristico = new PontoTuristico();
-        $pontoTuristico->setNome(readline("Nome: "))
+        elseif ($espacoEscolha == 2) {
+            $restaurante = new Restaurante();
+            $restaurante->setNome(readline("Nome: "))
                         ->setEndereco(readline("Endereço: "));
-        $pontoTuristico->setDuracaoVisita(readline("Duração da visita: "));
-        array_push($espacos, $pontoTuristico);
+            $restaurante->setTipoComida(readline("Tipo de comida: "));
+            array_push($espacos, $restaurante);
+            break; 
+        }
+
+        elseif ($espacoEscolha == 3) {
+            $pontoTuristico = new PontoTuristico();
+            $pontoTuristico->setNome(readline("Nome: "))
+                            ->setEndereco(readline("Endereço: "));
+            $pontoTuristico->setDuracaoVisita(readline("Duração da visita: "));
+            array_push($espacos, $pontoTuristico);
+            break;
+        }
+
+        else {
+            echo "Opção inválida! Tente novamente.\n";
+        }
     }
 }
 
+
+function excluirEspaco(&$espacos){
+    echo "EXCLUIR ESPAÇO";
+
+    if(count($espacos) == 0){
+        echo "Não há espacos cadastrados.\n";
+        return;
+    }
+
+    else{
+        foreach ($espacos as $chave => $esp){
+            echo "$chave. Nome: ". $esp->getNome(). "\n";
+        }
+
+        $indiceExcluir = readline("Digite o índice do Espaço que deseja excluir: ");
+
+        if (isset($espacos[$indiceExcluir])) {
+            array_splice($espacos, $indiceExcluir, 1);
+            echo "Espaço excluído com sucesso.\n";
+        }
+        else{
+            echo "Índice do espaço não encontrado.\n";
+        }
+    }
+}
+
+function listarEspacos(&$espacos){
+    if(count($espacos) == 0){
+        echo "Não há espacos cadastrados.\n";
+        return;
+    }
+
+    else{
+        foreach($espacos as $e){
+            if($e instanceof Hotel){
+                $e -> getDadosHotel();
+            }
+            else if($e instanceof Restaurante){
+                $e -> getDadosRestaurante();
+            }
+            else if($e instanceof PontoTuristico){
+                $e -> getDadosPontoTuristico();
+            }
+            else{
+                echo "Tipo de espaço desconhecido.\n";
+            }
+        }
+    }
+}
 
 
 while(true){
@@ -66,22 +126,28 @@ while(true){
     $num = readline("");
     switch($num){
         case 1:
-            
+            cadastrarEspaco($espacos);
             break;
+
         case 2:
-            
+            excluirEspaco($espacos);
             break;
+
         case 3: 
-        
+            listarEspacos($espacos);
             break;
+
         case 0:
-            
+            echo "Encerrendo programa";
             break;
+
         default: 
-        
+        echo "Opção inválida! Tente novamente...";
     }
 }
 ?>
+
+
 
 
 
