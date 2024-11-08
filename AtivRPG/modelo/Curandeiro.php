984ea47592a8e28596343c281825ee0ba578ec4d
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-11-08 15:34:10 -0300

    Create Curandeiro.php

---
+++ AtivRPG/modelo/Curandeiro.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Curandeiro extends Mago{
+
+    private int $forcaDeCura;
+    
+    public function lancarPoder(){
+        return "O mago ". $this->getNome(). ", do tipo Curandeiro possui força de ".
+        $this->forcaDeCura . "e lançou o poder " .  $this->poder->getDescricao() . "com força total de " .
+        $this->poder->getForcaTotal($this->forcaDeCura) . "\n";
+    }
+
+
+    /**
+     * Get the value of forcaDeCura
+     */
+    public function getForcaDeCura(): int
+    {
+        return $this->forcaDeCura;
+    }
+
+    /**
+     * Set the value of forcaDeCura
+     */
+    public function setForcaDeCura(int $forcaDeCura): self
+    {
+        $this->forcaDeCura = $forcaDeCura;
+
+        return $this;
+    }
+}
