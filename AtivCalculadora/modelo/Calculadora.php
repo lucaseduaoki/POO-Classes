6a0e795760f6782a5d9bd960472179dea5e0d6b9
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-12-12 15:11:59 -0300

    Create Calculadora.php

---
+++ AtivCalculadora/modelo/Calculadora.php
@@ -0,0 +1,45 @@
+<?php
+
+abstract class Calculadora{
+
+    protected $numA;
+    protected $numB;
+
+    abstract public function calcular();
+
+    /**
+     * Get the value of numA
+     */
+    public function getNumA()
+    {
+        return $this->numA;
+    }
+
+    /**
+     * Set the value of numA
+     */
+    public function setNumA($numA): self
+    {
+        $this->numA = $numA;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numB
+     */
+    public function getNumB()
+    {
+        return $this->numB;
+    }
+
+    /**
+     * Set the value of numB
+     */
+    public function setNumB($numB): self
+    {
+        $this->numB = $numB;
+
+        return $this;
+    }
+}
