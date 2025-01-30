da1fe71e8550dc824705dca67e1f2770c2adc85a
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-01-30 14:45:21 -0300

    Update ClientesDAO.php

--- AtivClientes/dao/ClientesDAO.php
+++ AtivClientes/dao/ClientesDAO.php
@@ -1,74 +1,76 @@
 <?php
-
 require_once("modelo/Cliente.php");
 require_once("modelo/ClientePF.php");
 require_once("modelo/ClientePJ.php");
+require_once("util/Conexao.php");
 
 class ClienteDAO {
-
-    //Método para inserir um cliente
     public function inserirCliente(Cliente $cliente) {
-        $sql = "INSERT INTO clientes 
-                (tipo, nome_social, email, nome, cpf, razao_social, cnpj)
-                VALUES
-                (?, ?, ?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO clientes(tipo, nome_social, email, nome, cpf, razao_social, cnpj) VALUES(?,?,?,?,?,?,?)";
 
         $con = Conexao::getCon();
-
-        $stm = $con->prepare($sql);
-        
+        $stmt = $con->prepare($sql);
         if($cliente instanceof ClientePF){
-            $stm->execute(array($cliente->getTipo(),
-                                $cliente-> getNomeSocial(),
-                                $cliente-> getEmail(),
-                                $cliente-> getNome(),
-                                $cliente-> getCpf(),
-                                null,
-                                null
-            ));
-        }
-        else if($cliente instanceof ClientePJ){
-            $stm->execute(array($cliente->getTipo(),
-                                $cliente-> getNomeSocial(),
-                                $cliente-> getEmail(),
-                                null,
-                                null,
-                                $cliente-> getRazaoSocial(),
-                                $cliente-> getCnpj(),
-            ));
-        }
+            $stmt->execute(array($cliente->getrTipo(), $cliente->getNomeSocial(), $cliente->getEmail(),$cliente->getNome(), $cliente->getCPF(), null, null));
+        } else if($cliente instanceof ClientePJ){
+            $stmt->execute(array($cliente->getrTipo(), $cliente->getNomeSocial(), $cliente->getEmail(), null, null, $cliente->getRazaoSocial(), $cliente->getCNPJ()));
+        }         
     }
 
-    public function listarClientes(){
-        $sql = "SELECT * FROM clientes";
+    public function listarClientes() {
+        $sql = "SELECT * FROM   clientes";
         $con = Conexao::getCon();
-        $stm = $con->prepare($sql);
-        $stm->execute();
-        $registros = $stm->fetchAll();
 
-        $clientes = $this->mapClientes($registros);
-        print_r($clientes);
+        $stmt = $con->prepare($sql);
+        $stmt->execute();
+        $registros = $stmt->fetchAll();
+        
+        return $this->MAPClientes($registros);
     }
 
-    private function mapClientes(array $registros){
-        
-        foreach($registros as $reg){
-            $cliente = null;
-            if($reg['tipo'] == 'F'){
+    private function MAPClientes(array $registros) {
+        $clientes = [];
+        foreach ($registros as $reg) {
+            if ($reg['tipo'] == 'F') {
                 $cliente = new ClientePF();
-                $cliente-> setNome ($reg['nome']);
-                $cliente-> setCpf ($reg['cpf']);
-            }
-            else{
+                $cliente->setNome($reg['nome']);
+                $cliente->setCPF($reg['cpf']);
+            } else {
                 $cliente = new ClientePJ();
-                $cliente-> setRazaoSocial ($reg['razao_social']);
-                $cliente-> setCnpj ($reg['cnpj']);
+                $cliente->setRazaoSocial($reg['razao_social']);
+                $cliente->setCNPJ($reg['cnpj']);
             }
             $cliente->setId($reg['id']);
             $cliente->setNomeSocial($reg['nome_social']);
             $cliente->setEmail($reg['email']);
-            array_push($clientes, $cliente);
+            $clientes[] = $cliente;
         }
         return $clientes;
     }
+
+    public function buscarClientes($id) {
+        $sql = "SELECT * FROM   clientes WHERE id = ?";
+        $con = Conexao::getCon();
+
+        $stmt = $con->prepare($sql);
+        $stmt->execute(array($id));
+        $registros = $stmt->fetchAll();
+        
+        return $this->MAPClientes($registros);
+
+        if(count($clientes) > 0)
+            return $clientes[0];
+        else
+            return null;
+    }
+
+    public function excluirCliente($id){
+        $sql = "DELETE FROM clientes WHERE id = ?";
+        $con = Conexao::getCon();
+    
+        $stmt = $con->prepare($sql);
+        $stmt->execute(array($id));
+    }
 }
+?>
+
