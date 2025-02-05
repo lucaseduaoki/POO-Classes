65baeeee677bc29cf68a7a6639c0b42ce9a886d9
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-02-04 22:13:58 -0300

    Create Produto.php

---
+++ AtivFilmes/modelo/Produto.php
@@ -0,0 +1,137 @@
+<?php 
+
+abstract class Produto{
+    protected string $nome;
+    protected string $dataLanc;
+    protected int $classIndicativa;
+    protected string $diretor;
+    protected string $genero;
+    protected bool $adptLivro;
+    protected bool $dispoWeb;
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dataLanc
+     */
+    public function getDataLanc(): string
+    {
+        return $this->dataLanc;
+    }
+
+    /**
+     * Set the value of dataLanc
+     */
+    public function setDataLanc(string $dataLanc): self
+    {
+        $this->dataLanc = $dataLanc;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of classIndicativa
+     */
+    public function getClassIndicativa(): int
+    {
+        return $this->classIndicativa;
+    }
+
+    /**
+     * Set the value of classIndicativa
+     */
+    public function setClassIndicativa(int $classIndicativa): self
+    {
+        $this->classIndicativa = $classIndicativa;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of diretor
+     */
+    public function getDiretor(): string
+    {
+        return $this->diretor;
+    }
+
+    /**
+     * Set the value of diretor
+     */
+    public function setDiretor(string $diretor): self
+    {
+        $this->diretor = $diretor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of genero
+     */
+    public function getGenero(): string
+    {
+        return $this->genero;
+    }
+
+    /**
+     * Set the value of genero
+     */
+    public function setGenero(string $genero): self
+    {
+        $this->genero = $genero;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of adptLivro
+     */
+    public function isAdptLivro(): bool
+    {
+        return $this->adptLivro;
+    }
+
+    /**
+     * Set the value of adptLivro
+     */
+    public function setAdptLivro(bool $adptLivro): self
+    {
+        $this->adptLivro = $adptLivro;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dispoWeb
+     */
+    public function isDispoWeb(): bool
+    {
+        return $this->dispoWeb;
+    }
+
+    /**
+     * Set the value of dispoWeb
+     */
+    public function setDispoWeb(bool $dispoWeb): self
+    {
+        $this->dispoWeb = $dispoWeb;
+
+        return $this;
+    }
+}
