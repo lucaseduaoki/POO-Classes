992aeaa4f94c50496a5c12f03b757c050d26c539
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-02-05 22:39:25 -0300

    Update Filme.php

--- AtivFilmes/modelo/Filme.php
+++ AtivFilmes/modelo/Filme.php
@@ -1,1 +1,111 @@
 
+<?php
+
+require_once("Produto.php");
+
+class Filme extends Produto {
+    private int $tempoDuracao;
+    private bool $continuidade;
+    private bool $cinema;
+    private bool $uniFisica;
+
+    public function getDadosProduto(){
+        
+        $dados = "Filme: ".$this ->getNome()."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Data de lançamento: ".$this->getDataLanc()."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Classificação indicativa: ". $this->getClassIndicativa()."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Diretor: ". $this->getDiretor()."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Gênero: ".$this-> getGenero()."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Adaptação de livro: ".($this -> adptLivro ? "Sim" : "Não")."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Tempo de duração: ".$this->getTempoDuracao()."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Continuidade: ". ($this -> continuidade ? "Sim" : "Não")."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Passou no cinema: ". ($this -> cinema ? "Sim" : "Não")."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Está disponível em meio físico: ". ($this -> uniFisica ? "Sim" : "Não")."\n";
+        $dados .="-----------------------------------------------------------------\n";
+        $dados .= "Está disponível em meio web: ". ($this -> dispoWeb ? "Sim" : "Não")."\n\n";
+        return $dados;
+    }
+
+
+
+    /**
+     * Get the value of tempoDuracao
+     */
+    public function getTempoDuracao(): int
+    {
+        return $this->tempoDuracao;
+    }
+
+    /**
+     * Set the value of tempoDuracao
+     */
+    public function setTempoDuracao(int $tempoDuracao): self
+    {
+        $this->tempoDuracao = $tempoDuracao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of continuidade
+     */
+    public function isContinuidade(): bool
+    {
+        return $this->continuidade;
+    }
+
+    /**
+     * Set the value of continuidade
+     */
+    public function setContinuidade(bool $continuidade): self
+    {
+        $this->continuidade = $continuidade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cinema
+     */
+    public function isCinema(): bool
+    {
+        return $this->cinema;
+    }
+
+    /**
+     * Set the value of cinema
+     */
+    public function setCinema(bool $cinema): self
+    {
+        $this->cinema = $cinema;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of uniFisica
+     */
+    public function isUniFisica(): bool
+    {
+        return $this->uniFisica;
+    }
+
+    /**
+     * Set the value of uniFisica
+     */
+    public function setUniFisica(bool $uniFisica): self
+    {
+        $this->uniFisica = $uniFisica;
+
+        return $this;
+    }
+}
