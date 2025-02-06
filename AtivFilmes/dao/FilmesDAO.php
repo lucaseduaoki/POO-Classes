df2be2297d9304cfe47e2614d493953a82787e7d
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-02-05 22:40:21 -0300

    Update FilmesDAO.php

--- AtivFilmes/dao/FilmesDAO.php
+++ AtivFilmes/dao/FilmesDAO.php
@@ -1,71 +1,105 @@
 <?php
 require_once("modelo/Produto.php");
 require_once("modelo/Filme.php");
-require_once("modelo/Série.php");
+require_once("modelo/Serie.php");
 require_once("util/Conexao.php");
 
-class ClienteDAO {
+class ProdutoDAO {
     public function inserirProduto(Produto $produto) {
-        $sql = "INSERT INTO clientes(tipo, nome_social, email, nome, cpf, razao_social, cnpj) VALUES(?,?,?,?,?,?,?)";
-
+        $sql = "INSERT INTO produtos (tipo, nome, data_lanc, class_indicativa, diretor, genero, adaptado_de_livro, disponivel_web, num_temporadas, temp_aprox_ep, num_aprox_ep, tempo_duracao, continuidade, cinema, unidade_fisica) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $con = Conexao::getCon();
         $stmt = $con->prepare($sql);
         if($produto instanceof Filme){
-            $stmt->execute(array($cliente->getrTipo(), $cliente->getNomeSocial(), $cliente->getEmail(),$cliente->getNome(), $cliente->getCPF(), null, null));
-        } else if($cliente instanceof ClientePJ){
-            $stmt->execute(array($cliente->getrTipo(), $cliente->getNomeSocial(), $cliente->getEmail(), null, null, $cliente->getRazaoSocial(), $cliente->getCNPJ()));
-        }         
+            $stmt->execute(array($produto->getTipo(),
+                                 $produto->getNome(),
+                                 $produto->getDataLanc(),
+                                 $produto->getClassIndicativa(),
+                                 $produto->getDiretor(),
+                                 $produto->getGenero(),
+                                 $produto->isAdptLivro(),
+                                 $produto->isDispoWeb(),
+                                 null,
+                                 null,
+                                 null,
+                                 $produto->getTempoDuracao(),
+                                 $produto->isContinuidade(),
+                                 $produto->isCinema(),
+                                 $produto->isUniFisica()
+                            ));
+                        }
+
+        else if($produto instanceof Serie){
+            $stmt->execute(array($produto->getTipo(),
+                                 $produto->getNome(),
+                                 $produto->getDataLanc(),
+                                 $produto->getClassIndicativa(),
+                                 $produto->getDiretor(),
+                                 $produto->getGenero(),
+                                 $produto->isAdptLivro(),
+                                 $produto->isDispoWeb(),
+                                 $produto->getNumTemporadas(),
+                                 $produto->getTempAproxEp(),
+                                 $produto->getNumAproxEp(),
+                                 null,
+                                 null,
+                                 null,
+                                 null 
+                            ));
+                        }
     }
 
-    public function listarClientes() {
-        $sql = "SELECT * FROM   clientes";
+    public function listarProdutos() {
+        $sql = "SELECT * FROM   produtos";
         $con = Conexao::getCon();
 
         $stmt = $con->prepare($sql);
         $stmt->execute();
         $registros = $stmt->fetchAll();
         
-        return $this->MAPClientes($registros);
+        return $this->MAPProduto($registros);
     }
 
-    private function MAPClientes(array $registros) {
-        $clientes = [];
+    private function MAPProduto(array $registros) {
+        $Produtos = [];
         foreach ($registros as $reg) {
             if ($reg['tipo'] == 'F') {
-                $cliente = new ClientePF();
-                $cliente->setNome($reg['nome']);
-                $cliente->setCPF($reg['cpf']);
+                $produto = new Filme();
             } else {
-                $cliente = new ClientePJ();
-                $cliente->setRazaoSocial($reg['razao_social']);
-                $cliente->setCNPJ($reg['cnpj']);
+                $cliente = new Serie();
             }
-            $cliente->setId($reg['id']);
-            $cliente->setNomeSocial($reg['nome_social']);
-            $cliente->setEmail($reg['email']);
-            $clientes[] = $cliente;
+            $produto->setId($reg['id']);
+            $produto->setTipo($reg['tipo']);
+            $produto->setNome($reg['nome']);
+            $produto->setDataLanc($reg['data_lanc']);
+            $produto->setClassIndicativa($reg['class_indicativa']);
+            $produto->setDiretor($reg['diretor']);
+            $produto->setGenero($reg['genero']);
+            $produto->setAdptLivro($reg['adaptado_de_livro']);
+            $produto->setDispoWeb($reg['disponivel_web']);
+
+        $produtos[] = $produto;
         }
-        return $clientes;
+        return $produtos;
     }
 
-    public function buscarClientes($id) {
-        $sql = "SELECT * FROM   clientes WHERE id = ?";
+    public function buscarProduto($id) {
+        $sql = "SELECT * FROM  produtos WHERE id = ?";
         $con = Conexao::getCon();
 
         $stmt = $con->prepare($sql);
         $stmt->execute(array($id));
         $registros = $stmt->fetchAll();
         
-        return $this->MAPClientes($registros);
+        return $this->MAPProduto($registros);
 
-        if(count($clientes) > 0)
-            return $clientes[0];
+        if(count($produtos) > 0)
+            return $produtos[0];
         else
             return null;
     }
 
-    public function excluirCliente($id){
-        $sql = "DELETE FROM clientes WHERE id = ?";
+    public function excluirProduto($id){
+        $sql = "DELETE FROM produto WHERE id = ?";
         $con = Conexao::getCon();
     
         $stmt = $con->prepare($sql);
