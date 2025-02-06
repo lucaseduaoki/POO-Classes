61ac22a6864391d99897f0c7ea35eb80ef38a2b0
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2025-02-06 15:51:40 -0300

    Update sql_banco.sql

--- AtivFilmes/sql_banco.sql
+++ AtivFilmes/sql_banco.sql
@@ -2,19 +2,19 @@
 
 CREATE TABLE produtos (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo VARCHAR(1), /* Filme (F) ou Série (S)*/
+    tipo VARCHAR(1) NOT NULL, /* Filme (F) ou Série (S)*/
     nome VARCHAR(255) NOT NULL,
     data_lanc DATE NOT NULL,
     class_indicativa INT NOT NULL,
-    diretor VARCHAR(100),
+    diretor VARCHAR(100) NOT NULL,
     genero VARCHAR(100) NOT NULL,
-    adaptado_de_livro BOOLEAN NOT NULL DEFAULT 0,
-    disponivel_web BOOLEAN NOT NULL DEFAULT 0,
+    adaptado_de_livro VARCHAR(5) NOT NULL DEFAULT 0,
+    disponivel_web VARCHAR(5) NOT NULL DEFAULT 0,
     num_temporadas INT,
     temp_aprox_ep INT,
     num_aprox_ep INT,
     tempo_duracao INT,
-    continuidade BOOLEAN NOT NULL DEFAULT 0,
-    cinema BOOLEAN NOT NULL DEFAULT 0,
-    unidade_fisica BOOLEAN NOT NULL DEFAULT 0,
+    continuidade VARCHAR(5),
+    cinema VARCHAR(5),
+    unidade_fisica VARCHAR(5)
 );
